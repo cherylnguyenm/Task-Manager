4c654c8876062f3edc8a9da5918752ee143e49b7
Chau Anh <chauanh.nguyenm@gmail.com>
2024-07-01 18:25:52 +0700

    Finished TaskCreate view + Init TaskUpdate view

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import TaskList, TaskDetail
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate
 
 urlpatterns = [
     path('', TaskList.as_view(), name='task-list'),  
     path('task/<int:pk>/', TaskDetail.as_view(), name='task-detail'),
+    path('task-create/', TaskCreate.as_view(), name='task-create'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView
+from django.urls import reverse_lazy
 from .models import Task
 
 class TaskList(ListView):
@@ -11,3 +13,13 @@ class TaskDetail(DetailView):
     template_name = 'base/task_detail.html'
     model = Task
     context_object_name = 'task_info'
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('task-list') # redirect users to task list
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('task-list')
